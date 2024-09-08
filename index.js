273f9635c806d0a7f436ee6b9ed44c8234b7e891
efurze <efurze@gmail.com>
2024-09-07 23:26:06 -0700

    Deleted almost everything

--- index.js
+++ index.js
@@ -9,34 +9,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Define a route for the root
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'chess.html'));
-});
-
-app.get('/hello', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'hello.html'));
-});
-
-app.get('/knight', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'knight.html'));
-});
-
-app.get('/game', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'game.html'));
+  res.sendFile(path.join(__dirname, 'public', 'scatter.html'));
 });
 
 app.get('/scatter', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'scatter.html'));
 });
 
-app.use('/public/js/third_party', express.static(__dirname + '/public/js/third_party/'));
 app.use('/data', express.static(__dirname + '/public/data'));
 
-/*
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-*/
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`app listening at http://localhost:${port}`);
 });
--- public/app.js
+++
@@ -1,7 +0,0 @@
-// public/app.js
-//import { Chess } from '/public/js/third_party/chess.js'
-//const chess = new Chess();
-
-export function sayHello() {
-	console.log('Hello, World from app.js!');
-}
